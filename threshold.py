a3ea7d2851de57d411ebd7f6668d51118923f534
khub41 <62697244+khub41@users.noreply.github.com>
2021-12-03 14:56:07 +0100

    Update threshold.py
    using logged model

--- threshold.py
+++ threshold.py
@@ -13,7 +13,7 @@ from sklearn.metrics import make_scorer, f1_score, matthews_corrcoef, roc_auc_sc
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-from udf import launch_models_CV, loss_score
+from udf import launch_models_CV, loss_score, determine_threshold
 
 # Import Data
 
@@ -42,31 +42,10 @@ scorings_gridsearch = {'f1': make_scorer(f1_score,
 
 print(labels_train.value_counts())
 
-best_model = LGBMClassifier(**{'class_weight': 'balanced',
-                               'folds': 3,
-                               'learning_rate': 0.05,
-                               'n_estimators': 500,
-                               'num_leaves': 10,
-                               'random_state': 41,
-                               'reg_alpha': 0.5,
-                               'reg_lambda': 0.5})
-
-best_model.fit(data_train, labels_train)
-
-labels_preds_df = pd.DataFrame(best_model.predict_proba(data_test), index=data_test.index)
-labels_preds_df['label_true'] = labels_test.values
-labels_preds_df['pred_thresh_5'] = best_model.predict(data_test)
-
-confusion = confusion_matrix(labels_preds_df['label_true'], labels_preds_df['pred_thresh_5'])
-
-baseline_score = loss_score(labels_preds_df['label_true'], labels_preds_df['pred_thresh_5'], data_appli_test=data_appli)
-f1_baseline = f1_score(labels_preds_df['label_true'], labels_preds_df['pred_thresh_5'], pos_label=1.0)
-
-
 logged_model = 'runs:/a4a40c98e82c4913a6e66558e4665c94/best_model'
 
-# Load model as a PyFuncModel.
 best_model_mlflow = mlflow.sklearn.load_model(logged_model)
+
 
 labels_preds_df = pd.DataFrame(best_model_mlflow.predict_proba(data_test), index=data_test.index)
 labels_preds_df['label_true'] = labels_test.values
@@ -77,24 +56,7 @@ confusion = confusion_matrix(labels_preds_df['label_true'], labels_preds_df['pre
 
 baseline_score = loss_score(labels_preds_df['label_true'], labels_preds_df['pred_thresh_5'], data_appli_test=data_appli)
 f1_baseline = f1_score(labels_preds_df['label_true'], labels_preds_df['pred_thresh_5'], pos_label=1.0)
+matthews_baseline = matthews_corrcoef(labels_preds_df['label_true'], labels_preds_df['pred_thresh_5'])
+roc_auc_baseline = roc_auc_score(labels_preds_df['label_true'], labels_preds_df['pred_thresh_5'])
 
-
-def determine_threshold(model, data_test, labels_test, range_thresh, data_appli):
-
-    labels_preds_df = pd.DataFrame(model.predict_proba(data_test), index=data_test.index)
-    labels_preds_df['label_true'] = labels_test.values
-    losses = {}
-    for threshold in range_thresh:
-
-        labels_preds_df[f'pred_thresh_{threshold}'] = labels_preds_df[1] > threshold
-        labels_preds_df[f'pred_thresh_{threshold}'].replace(True, 1.0)
-        labels_preds_df[f'pred_thresh_{threshold}'].replace(False, 0.0)
-        losses[threshold] = loss_score(labels_preds_df['label_true'],
-                                       labels_preds_df[f'pred_thresh_{threshold}'],
-                                       data_appli_test=data_appli)
-
-    return pd.Series(losses)
-
-
-
-
+losses, conf_matrix = determine_threshold(best_model_mlflow, data_test, labels_test, np.linspace(0,1,20,endpoint=True), data_appli)
