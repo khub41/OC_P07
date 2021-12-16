483a2e981af90485318f41c036a78d60710d0a5d
khub41 <62697244+khub41@users.noreply.github.com>
2021-12-16 17:41:31 +0100

    Update dash_board.py
    new plots

--- dash_board.py
+++ dash_board.py
@@ -7,14 +7,17 @@ from matplotlib import pyplot as plt
 import matplotlib as mpl
 import plotly.graph_objects as go
 
-
+st.set_page_config(layout="wide")
 @st.cache
 def load_scaled_data():
     return pd.read_csv("data/sample_test_scaled.csv", index_col=[0]).drop(columns=["TARGET"])
 
+
 @st.cache
 def load_raw_data():
     return pd.read_csv("data/data_full.csv", index_col=[0]).set_index('SK_ID_CURR')
+
+
 model_path = 'runs:/a4a40c98e82c4913a6e66558e4665c94/best_model'
 model = mlflow.sklearn.load_model(model_path)
 
@@ -28,59 +31,59 @@ data_raw = load_raw_data()
 # data_load_state.text('Loading data...done!')
 
 id_client = st.sidebar.selectbox(
-    "Client ID",
+    "ID du client",
     data_scale.index
 )
 
 risk = model.predict_proba(data_scale.loc[[id_client]])[0][1]
 
 strategy = st.sidebar.selectbox(
-    "Strategy",
-    ["Prudent", "Normal", "Aggressive"]
+    "Strategie",
+    ["Prudente", "Normale", "Aggressive"]
 )
 
 nb_features_explain = st.sidebar.slider(
-    label='Number of features to explain decision',
+    label='Nombre de critères pour expliquer la décision',
     min_value=1,
     max_value=20,
     value=5,
     step=1
 )
 
-st.title("Home Credit Decision Making Tool")
+st.title("Outil d'aide à la décision d'octroiement des prêts bancaires")
 
-if strategy.lower() == 'normal':
+if strategy.lower() == 'normale':
     threshold = 0.5
-    article = "a"
+    # article = "a"
 elif strategy.lower() == 'aggressive':
     threshold = 0.8
-    article = "an"
-elif strategy.lower() == 'prudent':
+    # article = "an"
+elif strategy.lower() == 'prudente':
     threshold = 0.2
-    article = "a"
+    # article = "a"
 else:
     threshold = 0.5
-    article = "a"
+    # article = "a"
 
 if risk > threshold:
     decision = 1
-    litteral_decision = "rejects"
+    litteral_decision = "rejette"
 else:
     decision = 0
-    litteral_decision = "accepts"
+    litteral_decision = "accepte"
 
 
 st.header(
-    f"With {article} {strategy.lower()} strategy, the model {litteral_decision.upper()} the client"
+    f"Avec une stratégie {strategy.lower()}, le modèle {litteral_decision.upper()} the client"
 )
 
 
-col_score, col_explanation = st.columns([2,1])
+col_score, col_explanation = st.columns(2)
+
 
 with col_score:
 
-
-    st.header("Risk")
+    st.header(f"Risque calculé {round(risk, 2)}")
 
     fig = go.Figure(go.Indicator(
         domain={'x': [0, 1], 'y': [0, 1]},
@@ -98,7 +101,8 @@ with col_score:
     st.plotly_chart(fig, use_container_width=True)
 
 with col_explanation:
-    st.header("Explanation")
+    st.header("Explication")
+    st.caption("Les variables bleues font baisser le risque, les rouges l'augmente!")
 
     explainer = shap.TreeExplainer(model)
 
@@ -115,18 +119,80 @@ with col_explanation:
                                                                                    'EXT_SOURCE_2',
                                                                                    'EXT_SOURCE_3'])]
 
-    if decision == 1:
-        # Reject the client
-        ascending_bool = False
-    else:
-        ascending_bool = True
+    # if decision == 1:
+    #     # Reject the client
+    #     ascending_bool = False
+    # else:
+    #     ascending_bool = True
 
-    explanation_client.sort_values('shap_value', ascending=ascending_bool, inplace=True)
+    explanation_client['shap_value_abs'] = explanation_client.shap_value.map(abs)
+    explanation_client['color'] = explanation_client.shap_value > 0
+    explanation_client.color.replace(True, 'red', inplace=True)
+    explanation_client.color.replace(False, 'blue', inplace=True)
+    explanation_client.sort_values('shap_value_abs', ascending=False, inplace=True)
 
 
     explanation_client = explanation_client.head(nb_features_explain)
-
+    explanation_client.sort_values('shap_value_abs', ascending=True, inplace=True)
     explanation_client['raw_data'] = data_raw_client[explanation_client.feature_name].iloc[0].values
+    explanation_client['bar_labels'] = explanation_client.feature_name + '\n=' \
+                                       + explanation_client.raw_data.round(2).astype(str)
 
-    st.dataframe(explanation_client)
+    fig = go.Figure(go.Bar(x=explanation_client['shap_value'],
+                     y=explanation_client['bar_labels'],
+                     orientation='h',
+                     marker={'color': explanation_client['color']},
+                     ),
+              )
+    fig.update_layout(xaxis_title="Influence sur le niveau de risque",
+                      )
 
+    st.plotly_chart(fig,
+
+
+        use_container_width=True)
+
+st.header('En savoir plus sur mon client')
+
+var_comparaison = st.selectbox(
+        "Variable à explorer",
+        data_raw.columns
+    )
+var_comparaison_value = data_raw.loc[id_client][var_comparaison]
+
+st.subheader(f"{var_comparaison}={var_comparaison_value}")
+if var_comparaison == 'index':
+    var_comparaison = "AMT_CREDIT"
+fig_comparaison = px.histogram(data_raw[var_comparaison])
+fig_comparaison.add_vline(var_comparaison_value,
+                          annotation_text=f'Client {id_client} \n {var_comparaison}={var_comparaison_value}',
+                          annotation_position="top right",
+                          line_dash="dot",
+                          line_color='green',
+                          line_width=3)
+fig_comparaison.update_layout(xaxis_title=var_comparaison,
+                              title='Repartition de la variable parmis les client')
+
+st.plotly_chart(fig_comparaison,
+                use_container_width=True)
+
+# col_var_x, col_var_y = st.columns(2)
+#
+# with col_var_x:
+#     var_x = st.selectbox(
+#         "Variable à tracer en X",
+#         data_raw.columns
+#     )
+#
+# with col_var_y:
+#     var_y = st.selectbox(
+#         "Variable à tracer en Y",
+#         data_raw.columns
+#     )
+#
+#
+# st.plotly_chart(
+#     go.Figure(go.Scatter(
+#                          x=data_raw[var_x],
+#                          y=data_raw[var_y]))
+# )
