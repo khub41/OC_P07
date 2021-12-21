606e5d47685baf97de497459a5ae9503ab134bd1
khub41 <62697244+khub41@users.noreply.github.com>
2021-12-21 10:49:59 +0100

    Update dash_board.py
    cleaning and comenting

--- dash_board.py
+++ dash_board.py
@@ -28,61 +28,42 @@ def load_raw_data():
         return pd.read_csv(file, index_col=[0]).drop(columns=["TARGET", "index"])
     # return pd.read_csv("data/data_full.csv", index_col=[0]).set_index('SK_ID_CURR')
 
-
-model_path = 'best_model/model.pkl'
-model = joblib.load(model_path)
-
-
-# Create a text element and let the reader know the data is loading.
-
-
-# data_load_state = st.text('Loading data...')
+# Importing Data from AWS s3
 data_scale = load_scaled_data()
 data_raw = load_raw_data()
-# data_load_state.text('Loading data...done!')
 
+# The user choses a client in the data base
 id_client = st.sidebar.selectbox(
     "ID du client",
     data_scale.index
 )
 
-# risk = model.predict_proba(data_scale.loc[[id_client]])[0][1]
+# Our API computes the risk. it posts the data about our client
 url = "https://homecredit-oc-p7.herokuapp.com/predict"
-# url = " http://127.0.0.1:12345/predict"
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 data_client = [data_scale.loc[id_client].values.tolist()]
 j_data = json.dumps(data_client)
 r = requests.post(url, data=j_data, headers=headers)
 risk = float(r.text.split('"')[1])
 
-
+# The user chooses the strategy (it sets the threshold accordingly)
 strategy = st.sidebar.selectbox(
     "Strategie",
     ["Prudente", "Normale", "Aggressive"]
 )
 
-nb_features_explain = st.sidebar.slider(
-    label='Nombre de critères pour expliquer la décision',
-    min_value=1,
-    max_value=20,
-    value=5,
-    step=1
-)
-
-st.title("Outil d'aide à la décision d'octroiement des prêts bancaires")
-
 if strategy.lower() == 'normale':
     threshold = 0.5
-    # article = "a"
+
 elif strategy.lower() == 'aggressive':
     threshold = 0.8
-    # article = "an"
+
 elif strategy.lower() == 'prudente':
     threshold = 0.2
-    # article = "a"
+
 else:
     threshold = 0.5
-    # article = "a"
+
 
 if risk > threshold:
     decision = 1
@@ -92,17 +73,30 @@ else:
     litteral_decision = "accepte"
 
 
+# The user chooses how many features he wants in the explanation graph
+nb_features_explain = st.sidebar.slider(
+    label='Nombre de critères pour expliquer la décision',
+    min_value=1,
+    max_value=20,
+    value=5,
+    step=1
+)
+
+# Title on the main page
+st.title("Outil d'aide à la décision d'octroiement des prêts bancaires")
+
+# Decision is displayed based on the risk and the strategy
 st.header(
     f"Avec une stratégie {strategy.lower()}, le modèle {litteral_decision.upper()} le client"
 )
 
-
+# Init of two columns, one for a gauge showing the risk, the other for the explanation bar graph
 col_score, col_explanation = st.columns(2)
 
 
 with col_score:
 
-    st.header(f"Risque calculé {round(risk, 2)}")
+    st.header(f"Risque calculé : {round(risk*100, 2)} %")
 
     fig = go.Figure(go.Indicator(
         domain={'x': [0, 1], 'y': [0, 1]},
@@ -124,50 +118,41 @@ with col_explanation:
     st.header("Explication")
     st.caption("Les variables vertes font baisser le risque, les rouges l'augmentent!")
 
+    # We need both scaled and raw data
     data_scale_client = data_scale.loc[[id_client]]
     data_raw_client = data_raw.loc[[id_client]]
     features = data_scale_client.columns.values
 
-    # explainer = shap.TreeExplainer(model)
-    # explainer_shap = shap.TreeExplainer(model)
-    #
-
-    # shap_values_client = explainer_shap.shap_values(data_scale_client)
-
+    # Sending the API the scaled data and getting a dict of the shap values
     url = "https://homecredit-oc-p7.herokuapp.com/explain"
-    # headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
     data_client = data_scale.loc[id_client].to_dict()
-    # j_data = json.dumps(data_client)
-    # j_data = {"array": data_client.tolist()}
     response_api = requests.post(url, json=data_client)
 
-
+    # We'll use a dataframe for convenience, sorting etc
     explanation_client = pd.DataFrame({'shap_value': response_api.json().values(),
                                        'feature_name': response_api.json().keys()})
 
+    # We drop lines that are not "explainable" (extern score) but very efficient!
     explanation_client = explanation_client[~explanation_client.feature_name.isin(['EXT_SOURCE_1',
                                                                                    'EXT_SOURCE_2',
                                                                                    'EXT_SOURCE_3'])]
-
-    # if decision == 1:
-    #     # Reject the client
-    #     ascending_bool = False
-    # else:
-    #     ascending_bool = True
-
+    # Getting most important lines using absolute values
     explanation_client['shap_value_abs'] = explanation_client.shap_value.map(abs)
+    # Tagging positive and negative values and setting a color for plotting
     explanation_client['color'] = explanation_client.shap_value > 0
     explanation_client.color.replace(True, 'red', inplace=True)
     explanation_client.color.replace(False, 'green', inplace=True)
+    # Sorting by abs value
     explanation_client.sort_values('shap_value_abs', ascending=False, inplace=True)
-
-
+    # Getting only the number asked by user
     explanation_client = explanation_client.head(nb_features_explain)
+    # Changing the order because plotly plots from bottom to top
     explanation_client.sort_values('shap_value_abs', ascending=True, inplace=True)
+    # Getting raw data and writing it on the labels
     explanation_client['raw_data'] = data_raw_client[explanation_client.feature_name].iloc[0].values
     explanation_client['bar_labels'] = explanation_client.feature_name + '\n=' \
                                        + explanation_client.raw_data.round(2).astype(str)
-
+    # Setup figure
     fig = go.Figure(go.Bar(x=explanation_client['shap_value'],
                      y=explanation_client['bar_labels'],
                      orientation='h',
@@ -182,19 +167,23 @@ with col_explanation:
 
         use_container_width=True)
 
+# Aditional information about client features
 st.header('En savoir plus sur mon client')
-
+# About a feature
 var_comparaison = st.selectbox(
         "Variable à explorer",
         data_raw.columns
     )
+# Getting the value and showing the value
 var_comparaison_value = data_raw.loc[id_client][var_comparaison]
-
 st.subheader(f"{var_comparaison}={var_comparaison_value}")
+# Init to AMT_CREDIT
 if var_comparaison == 'index':
     var_comparaison = "AMT_CREDIT"
-fig_comparaison = px.histogram(data_raw[var_comparaison])
 
+# Setup histogram
+fig_comparaison = px.histogram(data_raw[var_comparaison])
+# PLotting vertical line for position on clients feature compared to others
 if not np.isnan(var_comparaison_value):
     fig_comparaison.add_vline(var_comparaison_value,
                               annotation_text=f'Client {id_client} \n {var_comparaison}={var_comparaison_value}',
