607e8c244e9206b60421143de20d549723eff023
khub41 <62697244+khub41@users.noreply.github.com>
2021-12-22 15:04:15 +0100

    Update dash_board.py

--- dash_board.py
+++ dash_board.py
@@ -101,7 +101,7 @@ st.title("Outil d'aide à la décision d'octroiement des prêts bancaires")
 # st.header(
 #     f"Avec une stratégie {strategy.lower()}, le modèle {litteral_decision.upper()} le client"
 # )
-st.markdown(f"<center style=font-family:'IBM Plex Sans'; color:{color_decision}; font-size: 35px;'>{litteral_decision.upper()}</center>",
+st.markdown(f"<style> @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@700&display=swap'); <center style=font-family:'IBM Plex Sans', sans-serif; color:{color_decision}; font-size: 35px;'>{litteral_decision.upper()}</center></style>",
             unsafe_allow_html=True)
 
 # Init of two columns, one for a gauge showing the risk, the other for the explanation bar graph
